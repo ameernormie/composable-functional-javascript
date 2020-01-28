5dd03212ec5927184817a16eafe499ecf053af7b
Ameer Hamza <ameernormie@gmail.com>
2020-01-28 18:06:44 +0500

    feat: add chain method in either

--- src/Either/Either.js
+++ src/Either/Either.js
@@ -2,12 +2,14 @@
 
 const Right = x => ({
   map: f => Right(f(x)),
+  chain: f => f(x),
   fold: (f, g) => g(x),
   inspect: () => `Right(${x})`
 });
 
 const Left = x => ({
   map: f => Left(x),
+  chain: f => Left(x),
   fold: (f, g) => f(x),
   inspect: () => `Right(${x})`
 });
